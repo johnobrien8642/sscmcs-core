56cb72eb1291764ad2adb747449edc78c46aab0e
John O'Brien <johnobrien8642@gmail.com>
2024-08-02 17:12:41 -0500

    first commit

---
+++ components/system/Image.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Box, useBreakpointValue } from '@chakra-ui/react';
+import Image from 'next/image';
+import { AssetsType } from '../../pages/models/Assets';
+
+const MyImage = ({
+	image,
+	height,
+	setPhotoHook,
+	setOpenModalHook,
+	padding,
+	priority,
+	maxW
+}: {
+	image: AssetsType;
+	height?: number;
+	setPhotoHook?: React.Dispatch<React.SetStateAction<AssetsType | undefined>>;
+	setOpenModalHook?: React.Dispatch<React.SetStateAction<boolean>>;
+	padding?: number;
+	priority?: boolean;
+	maxW?: string;
+}) => {
+	const desktop = useBreakpointValue(
+		{
+			base: false,
+			md: true
+		}
+	)
+
+	return <Box
+		key={image._id.toString()}
+		width='100%'
+		maxW={maxW ?? '825px'}
+		padding={padding}
+		height={height}
+		m='auto'
+		sx={{
+			':hover': {
+				cursor: 'pointer'
+			},
+			img: {
+				width: '100%',
+				height: '100%',
+				objectFit: 'contain'
+			}
+		}}
+		alignContent='center'
+		objectFit='contain'
+		onClick={() => {
+			if (desktop) {
+				if (setPhotoHook) {
+					setPhotoHook(image)
+				}
+				if (setOpenModalHook) {
+					setOpenModalHook(true)
+				}
+			}
+		}}
+	>
+		<Image
+			priority={priority}
+			sizes='30w, (min-width: 520px) 65vw, (min-width: 1200px) 90vw'
+			alt={image.title || 'alt text'}
+			width={image?.assetDimensions[0] || image?.thumbnailDimensions[0]}
+			height={image?.assetDimensions[1] || image?.thumbnailDimensions[1]}
+			src={process.env.NEXT_PUBLIC_CLOUDFRONT_URL as string + (image.assetKey || image.thumbnailKey)}
+		/>
+	</Box>
+}
+
+export default MyImage;
---
+++ components/types/template_options.ts
@@ -0,0 +1,26 @@
+export const templateOptions = [
+	'Image Grid',
+	'Image Triptych',
+	'Photo List',
+	'Video Player',
+	'Book Cover CTA',
+	'Headline Only CTA',
+	'Text Block',
+	'About',
+	'PDF List',
+	'PDFView',
+] as const;
+
+export const assetTypes = [
+	'Image',
+	'Video',
+	'Text',
+	'PDF'
+] as const;
+
+export const textAlignOptions = [
+	'left',
+	'center',
+	'right',
+	''
+] as const;
